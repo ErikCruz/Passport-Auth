9fe15d40e3d6aecc0176092ede831892f1b279ec
Erik Cruz <erikc1989@live.com>
2015-06-24 03:04:19 +0000

    Add required modules for authentication

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require("express-session");
+
+var passport = require('passport');
+var passportLocal = require('passport-local');
 
 var app = express();
 
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(expressSession({
+    secret: 'mySecret'
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function(req, res){
    res.render('index', {
@@ -13,6 +29,10 @@ app.get('/', function(req, res){
 
 app.get('/login', function(req, res){
    res.render('login'); 
+});
+
+app.post('/login', function(req, res){
+    
 });
 
 var port = process.env.PORT || 3000;
